de63d5a0314a1fde9a21650b55ed1936e6caeb9f
Danny Cao <dannycao1997@yahoo.com>
2024-06-13 17:25:29 -0400

    connect mongodb to port 8080

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
@@ -12,8 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+
 
 app.get('/', async (req, res) => {
     res.status(200).json({
