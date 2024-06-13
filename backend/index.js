e0b08c1ee019ce66b1843c06fbbd6d49459d3ceb
Danny Cao <dannycao1997@yahoo.com>
2024-06-13 17:29:29 -0400

    create dalleroutes and postroutes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
 
 dotenv.config();
 
@@ -10,7 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
     res.status(200).json({
